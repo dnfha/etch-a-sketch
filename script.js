16b3db66709467b3abd2dcfd595f7e4d3f8186fa
dnfha <31221487+dnfha@users.noreply.github.com>
2025-02-27 14:35:42 +0000

    Utilize user prompt function to determine grid size
    Create a function to take user input and validate the input value.
    Empty the grid container and create new boxes according to user input.
    Show the current grid size on top of the grid container.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function createGrid(gridCount = 16) {
-  const gridContainer = document.querySelector("#grid-container");
-  const rows = gridCount;
-  const cols = gridCount;
+const gridContainer = document.querySelector("#grid-container");
+
+const button = document.querySelector("button");
+
+function updateGridSizeText(gridSize) {
+  const gridSizeText = document.querySelector("#grid-size-text");
+  gridSizeText.textContent = `Grid: ${gridSize} x ${gridSize}`;
+}
+
+function createGrid(gridSize = 16) {
+  const rows = gridSize;
+  const cols = gridSize;
   totalBoxes = rows * cols;
 
   for (let i = 0; i < totalBoxes; i++) {
@@ -12,14 +20,37 @@ function createGrid(gridCount = 16) {
     box.style.minHeight = `${boxSize}px`;
     gridContainer.appendChild(box);
   }
+
+  updateGridSizeText(gridSize);
 }
 
-document.addEventListener("DOMContentLoaded", createGrid(32));
+function clearGrid() {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.remove();
+  });
+}
 
-const boxes = document.querySelectorAll(".box");
+document.addEventListener("DOMContentLoaded", createGrid());
 
-boxes.forEach((box) => {
-  box.addEventListener("mouseout", (e) => {
-    e.target.classList.add("colored");
+button.addEventListener("click", () => {
+  let gridSize;
+  while (gridSize !== null) {
+    gridSize = window.prompt("Enter grid size (min 16, max 100)", 16);
+    console.log(gridSize);
+    if (Number(gridSize) >= 16 && Number(gridSize) <= 100) {
+      clearGrid();
+      createGrid(gridSize);
+      break;
+    }
+  }
+});
+
+gridContainer.addEventListener("mouseenter", () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseout", (e) => {
+      e.target.classList.add("colored");
+    });
   });
 });
